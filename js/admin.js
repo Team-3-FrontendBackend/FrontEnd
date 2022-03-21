435138f459370ba7b6a35de5bdbcdd4ca4389006
Stockton Cannon <77078756+StocktonCannon@users.noreply.github.com>
2022-03-20 18:21:11 -0600

    Base Login functionality

---
+++ js/admin.js
@@ -0,0 +1,32 @@
+import ExternalServices from './ExternalServices.js';
+
+export default class Admin {
+  constructor() {
+    this.token = null;
+    this.services = new ExternalServices();
+  }
+  async login(creds, next) {
+    // I built the login method with a callback: next. This makes it much more flexible...
+    // there could be many different things the user wants to do after logging in...
+    // this allows us that flexibility without having to write a bunch of login methods
+    try {
+      this.token = await this.services.loginRequest(creds);
+      next()
+    } 
+    catch(err) {
+      console.log(err)
+    }
+  }
+  setLogin() {
+    // add the html for the login form
+      // now that it is in the DOM we can add a listener for the login button
+      document.querySelector('#loginButton').addEventListener('click', (e) => {
+        const username = document.querySelector('#username').value;
+        const password = document.querySelector('#password').value;
+        console.log(username)
+        console.log(password)
+        this.login({username, password});
+      });
+  }
+  
+}
