644ec53bcf17487f15b0176795892f58aefdf2e6
Stockton Cannon <77078756+StocktonCannon@users.noreply.github.com>
2022-03-23 10:21:42 -0600

    Finished session storage for JWT,  added some error handling

--- js/user.js
+++ js/user.js
@@ -5,16 +5,19 @@ export default class User {
     this.token = null;
     this.services = new ExternalServices();
   }
-  async login(creds, next) {
+  async login(creds) {
     // I built the login method with a callback: next. This makes it much more flexible...
     // there could be many different things the user wants to do after logging in...
     // this allows us that flexibility without having to write a bunch of login methods
     try {
       this.token = await this.services.loginRequest(creds);
-      next()
+      window.sessionStorage.setItem('creds',this.token)
+      
     } 
     catch(err) {
       console.log(err)
+      const messageOutput = document.getElementById("errMessage");
+      messageOutput.innerHTML = err.message.message;
     }
   }
   setLogin() {
