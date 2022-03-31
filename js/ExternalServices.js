00e1dafbcabd00299cb714b0874bf35995e26700
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-03-30 18:47:13 -0600

    Added some of the JS for the API call

--- js/ExternalServices.js
+++ js/ExternalServices.js
@@ -1,6 +1,5 @@
 const baseURL = 'https://cms-societies.herokuapp.com/';
 
-
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
@@ -25,6 +24,25 @@ export default class ExternalServices  {
       body: JSON.stringify(user)
     }
     const response = await fetch(baseURL + 'login', options).then(convertToJson);
+    return response;
+  }
+}
+
+export class EditorExternalServices  {
+  constructor() {
+  }
+  async updatePage(pageData, token) {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        // pass in authorization token here
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(pageData)
+    }
+    // Figure out how to pass the name to the API, maybe use local storage
+    const response = await fetch(baseURL + 'admin/', options).then(convertToJson);
     return response;
   }
 }
