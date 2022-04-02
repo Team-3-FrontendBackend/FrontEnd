d6930dc6f82c40eb1f4013218e1dc9cf7fdb3a65
dandlp95 <del17011@byui.edu>
2022-04-02 02:24:20 -0600

    getValues function of pageEditor class

--- js/pageEditor.js
+++ js/pageEditor.js
@@ -1,6 +1,20 @@
 import ExternalServices from './ExternalServices.js';
 
-class PageEditor {
+function htmlRenderer(title, text, imgBase64) {
+    
+    let sectionDiv = `<div class="section">
+    <h2>${title}</h2>
+    <p>${text}</p>`;
+
+    if(typeof imgBase64 != undefined){
+        sectionDiv += `<img src="${imgBase64}">`;
+    }
+    
+    sectionDiv += `</div>`;
+
+}
+
+export default class PageEditor {
     constructor(siteName) {
         this.token = null;
         this.services = new ExternalServices('admin/' + siteName);
@@ -11,25 +25,24 @@ class PageEditor {
 
     }
     getValues(){
+        document.querySelector('#submitBtn').addEventListener('click', (e)=>{
 
+            let sectionTitle = document.querySelector('#title').value;
+            let sectionText = document.querySelector('#text').value;
 
-        let sectionTitle = document.querySelector('#title').value;
-        let sectionText = document.querySelector('#text').value;
-        
-        let pageData = {
-            
-        };
+            e.preventDefault();
 
-        this.services.updatePageRequest();
+            let upload = document.querySelector('#image');
+            let img = upload.files[0];
+
+            const reader = new FileReader();
+            reader.readAsDataURL(img);
+
+            reader.addEventListener('load', ()=>{
+                let imgBase64 = reader.result;
+                console.log(JSON.stringify({sectionTitle, sectionText, imgBase64}));
+            })
+        })
     }
 }
-
-function htmlRenderer(title, text, image) {
-    
-
-    if(typeof image != undefined){
-        
-    }
-}
-
 
